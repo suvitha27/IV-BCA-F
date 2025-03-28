8987ff565fedd15e0fc241ad7c7a198997f2ba38
suvi <suvithapaul57@gmail.com>
2025-03-28 11:49:23 +0530

    Update python.py
    change in function

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ num1 = int(input('Enter first number: '))
 num2 = int(input ('Enter second number: '))
 
 # Add two numbers
-sum = float(num1 + num2)
+sum = float(num1 - num2)
 
 # Display the sum
 print(f'The sum of {num1} and {num2} is {sum}')
